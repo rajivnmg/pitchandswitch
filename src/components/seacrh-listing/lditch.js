63ace01a5cc7e48a565c5eb4a1d8a0f4b0f953c3
karnika <karnika.sharma@nmg-d-47.newmediaguruorg.com>
2018-10-12 18:13:55 +0530

    updated

--- src/components/seacrh-listing/lditch.js
+++ src/components/seacrh-listing/lditch.js
@@ -24,26 +24,71 @@ class viewPitchPopup extends Component {
 			offerTrade:this.props.offerTrade,
 			offerTradeProducts:[]
 		}				
-	 }   
-	  
-	  conditionsChange = (objectSelectedPitch) => {
-          this.setState({categoriesValues: objectSelectedPitch.target.value});
-          //console.log('categoriesValues',this.state.categoriesValues);
-           axios.get('/trade/getProductByCategory/'+this.state.categoriesValues).then(result => {
+	 }  
+	 
+	 
+	 handleFormInputChange(e) {
+		  var el = e.target.value
+			var name = el.name
+			var type = el.type
+			 var stateChange = {}		
+			  var l = 3;
+			   var selectedOptions = []
+				for(var i = 1; i <= l; i++) {					
+				    //console.log('asdf',el);
+					selectedOptions.push(el)
+					console.log('selectedOptions',selectedOptions)
+			    }
+			  //~ else if (type == 'checkbox') {
+				//~ var objType = Object.prototype.toString.call(el.form.elements[name])
+				//~ if (objType == '[object RadioNodeList]' || objType == '[object NodeList]' || objType == '[object HTMLCollection]') {
+				  //~ var checkedBoxes = (Array.isArray(this.state[name]) ? this.state[name].slice() : [])
+				  //~ if (el.checked) {
+					//~ checkedBoxes.push(el.value)
+				  //~ }
+				  //~ else {
+					//~ checkedBoxes.splice(checkedBoxes.indexOf(el.value), 1)
+				  //~ }
+				  //~ stateChange[name] = checkedBoxes
+				//~ }
+				//~ else {
+				  //~ stateChange[name] = el.checked
+				//~ }
+			  //~ }
+			  //~ else {
+				//~ stateChange[name] = el.value
+			  //~ }
+//~ 
+			  //~ this.setState(stateChange)
+        }
+	 
+	 
+	 
+	 
+	 handleOnChange = (chosenValue) => {
+		 console.log('chosenValue',chosenValue.target.value)
+           this.setState({ categoriesValues: chosenValue.target.value})
+		     axios.get('/trade/getProductByCategory/'+chosenValue.target.value).then(result => {
 			 if(result.data.code === 200){
-			   this.setState({offerTradeProducts:result.data.result})	
-		    }
-		})
-      }
-	
+			    this.setState({getAllProduct:result.data.result})	
+			    console.log('getAllProduct',this.state.getAllProduct);
+		        }
+		    })
+      } 
+	  
+	 
 	componentWillMount(){
 		this.setState({offerTradeId:this.props.offerTrade._id})
 		   axios.get('/trade/getAllProduct/').then(result => {
 			  if(result.data.code === 200){
 			    this.setState({getAllProduct:result.data.result})				
-			     //console.log('getAllProduct',this.state.getAllProduct)
 		    }
 		})
+		axios.get('/category/categoriesActive/').then(result => {
+			if(result.data.code === 200){
+			  this.setState({categoryActive:result.data.result})				
+		   }
+		})	
 	}
 	
 	componentDidMount(){
@@ -51,24 +96,18 @@ class viewPitchPopup extends Component {
 			if(result.data.code === 200){
 			  this.setState({offerTradeProducts:result.data.result})
 		   }
-		})		
-		
-				
-		axios.get('/category/categoriesActive/').then(result => {
-			if(result.data.code === 200){
-			  this.setState({categoryActive:result.data.result})				
-			  //console.log('categoryActive',this.state.categoryActive);
-		   }
-		})		
+		})	
 	}
+	
 
    render() {
 	 let optionTemplate;
 	    if(this.state.categoryActive){
-			let conditionsList = this.state.categoryActive;
+		let conditionsList = this.state.categoryActive;
 			console.log('conditionsList',conditionsList)
 		    optionTemplate = conditionsList.map(v => (<option key={v._id} value={v._id}>{v.title}</option>));
        }
+       
     let img = this.props.offerTrade.userId?this.props.offerTrade.userId.profilePic:"";
     let productImg = 
     this.props.offerTrade.productImages?this.props.offerTrade.productImages[0]:"";
@@ -81,44 +120,51 @@ class viewPitchPopup extends Component {
         <a className="close" onClick={close}>
             &times;
         </a>
-        <div className="header">Choose products to <span className="yellow">Pitch</span> on 
-        <div className="select-box top-right">
-             <select id="select" innerRef={input => (this.condition = input)} className="form-control" onChange={this.conditionsChange}>
-				{optionTemplate}
-			</select>
-		</div>
-		<div className="cl"></div>
-		</div>
-			<div className="content">
-			<div className="received-product">
-				<div className="received-product-box">
-					<div className="received-product-image-box">
-					<img src={constant.BASE_IMAGE_URL+'Products/'+productImg} alt="recieved-product image" />
+        <form className="pure-form pure-form-stacked" onChange={this.onChange}>
+			<div className="header">Choose products to <span className="yellow">Pitch</span> on 
+			<div className="select-box top-right">
+				 <select id="select"  onChange={this.handleFormInputChange}>
+					{optionTemplate}
+				</select>
+			</div>
+			<div className="cl"></div>
+			</div>
+				<div className="content">
+				<div className="received-product">
+					<div className="received-product-box">
+						<div className="received-product-image-box">
+						<img src={constant.BASE_IMAGE_URL+'Products/'+productImg} alt="recieved-product image" />
+						</div>
+						<div className="received-product-content-box">
+							<span>Product ID: <strong>{this.props.offerTrade._id}</strong></span>
+							<h4>{this.props.offerTrade.productName}</h4>
+							  <a className="catLink" href="/">{this.props.offerTrade.description}</a>
+								<div className="ratingRow">
+								<div className="pic"><img src={constant.BASE_IMAGE_URL+'ProfilePic/'+img} alt="" /></div>
+								<p>{this.props.offerTrade.description}</p>
+								<div className="rated">4</div>
+								<div className="cl"></div>
+							</div> 
+						</div>
 					</div>
-					<div className="received-product-content-box">
-						<span>Product ID: <strong>{this.props.offerTrade._id}</strong></span>
-						<h4>{this.props.offerTrade.productName}</h4>
-						<a className="catLink" href="/">{this.props.offerTrade.description}</a>
-							<div className="ratingRow">
-							<div className="pic"><img src={constant.BASE_IMAGE_URL+'ProfilePic/'+img} alt="" /></div>
-							<p>{this.props.offerTrade.description}</p>
-							<div className="rated">4</div>
-							<div className="cl"></div>
-						</div> 
-					</div>
-				</div>
-				<div className="cl"></div>
-				  <div className="switch-product-section choose-product-div border-top">
+					<div className="cl"></div>
+					  <div className="switch-product-section choose-product-div border-top">
 						<Scrollbars className="Scrollsdiv" style={{height: 585 }}>
-						  <If condition={this.state.getAllProduct.length > 0}>
+						 <If condition={this.state.getAllProduct.length > 0}>
 							<Then>
 							{ this.state.getAllProduct.map((productsListing, index) => {
+								var count = index+1;
 								var productImages = (productsListing.productImages)?(productsListing.productImages[0]):'';
 								return(
 								<div className="switch-product-box">
 								<div className="switch-product-image-box">
 								 <img src={constant.BASE_IMAGE_URL+'Products/'+productImages} alt="recieved-product image" />
-								 <div className="switch-option-mask"> <div className="check-box"><input id="pitch1" type="checkbox" /><label htmlFor="pitch1">&nbsp;</label></div> </div>
+								 <div className="switch-option-mask">
+									<div className="check-box">
+									   <input id={"pitch"+count} type="checkbox" name="productIDS"  value={productsListing._id} onChange={this.handleFormInputChange}/>
+										 <label htmlFor={"pitch"+count}>&nbsp;</label>
+									   </div>
+									 </div>
 								</div>
 								<div className="switch-product-content-box">
 								  <h4>{productsListing.productName?productsListing.productName:""}</h4>
@@ -128,19 +174,20 @@ class viewPitchPopup extends Component {
 								  )
 								})
 							  }
-							</Then>							
+						  </Then>							
 							<Else>
 							   <p>No Data Available</p>
 							</Else>
 							</If>
-						   </Scrollbars>
-						  <div className="btm-btns">
-						<a className="more-items" href="#">Pitch Now</a>
-					   <a className="ditch cancel-ditch"> Cancel Pitch </a>
-					 </div>
+							 </Scrollbars>
+							  <div className="btm-btns">
+							   <a className="more-items" href="#">Pitch Now</a>
+						   <a className="ditch cancel-ditch"> Cancel Pitch </a>
+						 </div>
+					</div>
 				</div>
 			</div>
-		</div>
+		 </form>
       </div>
     )}
 </Popup>
